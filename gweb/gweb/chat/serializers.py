47cf34fb67f32f0d7f8f6ac61812376f9e62c5ea
Mcmanu007 <agyekum006felix@gmail.com>
2025-07-18 07:35:53 +0000

    serializers.py
    Signed-off-by: Mcmanu007 <agyekum006felix@gmail.com>

--- gweb/gweb/chat/serializers.py
+++ gweb/gweb/chat/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 from .models import Chat, ChatHistory
-from .models import AudioFile, TextToSpeech
+from .models import TextToSpeech
 
 class ChatHistorySerializer(serializers.ModelSerializer):
     class Meta:
